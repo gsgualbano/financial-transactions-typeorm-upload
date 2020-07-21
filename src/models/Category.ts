f7b782a5c1ac6c9f6db0d1cf5ec9703fcb412ad1
Gustavo Gualbano <gustavogualbano@MacBook-Pro-de-Gustavo.local>
2020-07-21 12:37:13 -0400

    feat(models): add Category and Transaction

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,11 +1,29 @@
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Column,
+  Entity,
+  OneToMany,
+  UpdateDateColumn,
+} from 'typeorm';
+import Transaction from './Transaction';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transactions: Transaction[];
 }
 
 export default Category;
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  JoinColumn,
+  CreateDateColumn,
+  ManyToOne,
+  Entity,
+  UpdateDateColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn()
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
