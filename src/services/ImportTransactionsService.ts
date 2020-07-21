c71e048e371b5b7a5c16139baafef51dee3441a0
Gustavo Gualbano <gustavogualbano@MacBook-Pro-de-Gustavo.local>
2020-07-21 12:42:35 -0400

    feat(services): add import transactions service

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,58 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
+
+interface CsvProps {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(csvFilePath: string): Promise<Transaction[]> {
+    const createTransaction = new CreateTransactionService();
+    const readCSVStream = fs.createReadStream(csvFilePath);
+
+    const parseStream = csvParse({
+      columns: true,
+      skip_empty_lines: true,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const lines: CsvProps[] = [];
+
+    parseCSV.on('data', line => {
+      lines.push(line);
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    const transactions = [];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const line of lines) {
+      const { title, value, type, category } = line;
+
+      // eslint-disable-next-line no-await-in-loop
+      const transaction = await createTransaction.execute({
+        title,
+        value,
+        type,
+        categoryTitle: category,
+      });
+
+      transactions.push(transaction);
+    }
+
+    return transactions;
   }
 }
 
